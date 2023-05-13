7bbed2aa54bc88ae0314b137a27b0281f5d25614
Ryan-Pi <68978091+Ryan-Pi@users.noreply.github.com>
2023-05-13 21:00:05 +1000

    Creation and initial planning of script

---
+++ save_backup.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import shutil
+import argparse
+import datetime
+
+# python script with config file
+
+def main():
+    # path to find saves
+    # path to backup saves
+    # command to list games, with path settings saved?
+    # command to add games with path settings
+    # create folder in specified backup directory, with saves, named date + time
+    print("")
+    
+
+def add():
+    #use Tuple to save game name, save path, backup path?
+    print("")
+
+def change():
+    print("")
+    
+def save(game):
+    print("")    
+def load(game):
+    #
+    print("")
